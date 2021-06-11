edb0a3fb0fd00a1fe1cf3a2cf571c3db1c88f769
yang_fu <yangfu@i72.com>
2021-06-11 16:33:31 +0800

    feat: vue3-ts-vite

---
+++ vue-vite-ts-demo/src/components/HelloWorld.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>3</div>
+</template>
+
+<script lang="ts">
+import { ref, defineComponent } from 'vue'
+export default defineComponent({
+  name: 'HelloWorld',
+  props: {
+    msg: {
+      type: String,
+      required: true
+    }
+  },
+  setup: () => {
+    const count = ref(0)
+    return { count }
+  },
+  created() {
+    this.testFn()
+  },
+  methods: {
+    testFn() {
+      function sortA<T>(arr: T[]):T[] {
+        return arr
+      }
+      sortA([])
+    }
+  }
+})
+</script>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+
+label {
+  margin: 0 0.5em;
+  font-weight: bold;
+}
+
+code {
+  background-color: #eee;
+  padding: 2px 4px;
+  border-radius: 4px;
+  color: #304455;
+}
+</style>
